ba8a433d773b3fbaa95d700740b9175654aa538f
Stephen McKillop <smckillop@innovada.ca>
2020-03-30 15:25:02 -0400

    functions

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,100 @@ const db = admin.firestore()
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
+exports.addToSessionInfo = functions.https.onRequest((req, res) => {
+
+    var userID = req.body.uid;
+
+    admin.firestore().collection("sessions").get()
+    .then(sessions => {
+        sessions.forEach(doc => {
+            if (doc.id != userID) {
+                var inf = doc.data().info
+                console.log("collection: sessions /// doc: " + doc.id)
+                if (inf.indexOf(userID) == -1) {
+                    inf.push(userID)
+                }
+                admin.firestore().collection("sessions").doc(doc.id).set({info : inf})
+                .then(added => {
+                    console.log("added to list")
+                })
+                .catch(err => {
+                    console.log(err)
+                    return res.sendStatus(500)
+                })
+            }
+        })
+
+        return res.sendStatus(200)
+    })
+    .catch(err => {
+        console.log(err)
+        return res.sendStatus(500)
+    })
+})
+exports.removeFromSessionInfo = functions.https.onRequest((req, res) => {
+    var userID = req.body.uid;
+
+    admin.firestore().collection("sessions").get()
+    .then(sessions => {
+        sessions.forEach(doc => {
+            if (doc.id != userID) {
+                var inf = doc.data().info
+                console.log("collection: sessions /// doc: " + doc.id)
+                inf.splice(inf.indexOf(userID), 1)
+                admin.firestore().collection("sessions").doc(doc.id).set({info : inf})
+                .then(added => {
+                    console.log("added to list")
+                })
+                .catch(err => {
+                    console.log(err)
+                    return res.sendStatus(500)
+                })
+            }
+        })
+
+        return res.sendStatus(200)
+    })
+    .catch(err => {
+        console.log(err)
+        return res.sendStatus(500)
+    })
+})
+exports.getFirst = functions.https.onRequest((req, res) => {
+    var userID = req.body.uid
+    var count = req.body.count
+    admin.firestore().collection("sessions").doc(userID).get()
+        .then(data => {
+            if (count > data.data().info.length - 1) {
+                return res.send(data.data().info)
+            }
+            console.log(data.data().info[index])
+            return res.sendStatus(200)
+        })
+        .catch(err => {
+            console.log(err)
+            return res.sendStatus(500)
+        })
+})
+exports.getNext = functions.https.onRequest((req, res) => {
+
+    var userID = req.body.uid
+    var index = req.body.index
+    admin.firestore().collection("sessions").doc(userID).get()
+        .then(data => {
+            if (index > data.data().info.length - 1) {
+                return res.sendStatus(201)
+            }
+            console.log(data.data().info[index])
+            return res.sendStatus(200)
+        })
+        .catch(err => {
+            console.log(err)
+            return res.sendStatus(500)
+        })
+
+})
+
 exports.setupSessionTable = functions.https.onRequest((request, response) => {
 
     admin.firestore().collection("users").get()
